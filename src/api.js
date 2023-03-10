bd6c2257033432bcbd027ac34f5747bf29eb2394
ronaldo <mafronfernandes28@gmail.com>
2023-03-11 00:23:39 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6c873e6d3emsh7b29e96851fab72p1be4cejsnb7f0501a83b7",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "cf2cfa38f7731361c4211736a28da942"; // enter your key from openweather API
