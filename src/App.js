4e7d2d0359f09dccdcddd360075aab199af3f5da
ronaldo <mafronfernandes28@gmail.com>
2023-03-11 20:29:07 +0530

    Adding Developer Name

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 import Forecast from "./components/forecast/forecast";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import Developer from "./components/developer";
 import "./App.css";
 
 function App() {
@@ -35,6 +36,7 @@ function App() {
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <Forecast data={forecast} />}
+      <Developer />
     </div>
   );
 }
---
+++ src/components/developer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Developer() {
+  return (
+    <div>
+      <h1>Developed by Mafron Ronaldo Fernandes</h1>
+    </div>
+  );
+}
+
+export default Developer;
